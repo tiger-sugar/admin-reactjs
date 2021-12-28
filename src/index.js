be05126da1d2d5edd478aa4f436c947a69f0afaf
HIEU PHAM <ptrunghieu2607@gmail.com>
2021-12-28 17:27:02 +0700

    update stayed login

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import store from "./redux/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 // import * as serviceWorker from "./serviceWorker";
+import configureStore from "./redux/configureStore";
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById("root")
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,16 +4,27 @@ import myProfileReducer from "./saga-modules/myProfile/reducer";
 import authReducer from "./saga-modules/auth/reducer";
 import rootSagas from "./sagas";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 const reducer = combineReducers({
   myProfile: myProfileReducer,
   auth: authReducer,
 });
+const persistConfig = {
+  key: "root",
+  storage,
+};
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
-sagaMiddleware.run(rootSagas);
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export default () => {
+  const store = createStore(
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+  );
+  sagaMiddleware.run(rootSagas);
+
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
